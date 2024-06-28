60c4976e3a55dcc2d558014029d080887486e5a8
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-28 23:42:50 +0300

    Removed the ensure attr

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # Creates a file `/tmp/school`, with the following params
 
 file { '/tmp/school':
-    ensure  => 'file',
     path    => '/tmp/school',
     content => 'I love puppet',
     mode    => '0744',
