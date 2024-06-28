be559161aa472873deeed7436ac63986607d9b7d
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-29 00:15:55 +0300

    Removed the path attr

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # Creates a file `/tmp/school`, with the following params
 
 file { '/tmp/school':
-    path    => '/tmp/school',
     content => 'I love puppet',
     mode    => '0744',
     group   => 'www-data',
