f5802a5c1013e49303737a4ec7ab0093287f1ed5
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-06-29 00:16:45 +0300

    Fixed a typo in 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Creates a file `/tmp/school`, with the following params
 
 file { '/tmp/school':
-    content => 'I love puppet',
+    path    => '/tmp/school',
+    content => 'I love Puppet',
     mode    => '0744',
     group   => 'www-data',
     owner   => 'www-data',
